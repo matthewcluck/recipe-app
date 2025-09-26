1fe15c305045e6ade980550b062c5bfaafb464e8
Matthew Cluck <53344005+matthewcluck@users.noreply.github.com>
2025-09-26 16:31:52 +0000

    chore: fixed tests

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-import { PostList } from './components/PostList.jsx'
-import { CreatePost } from './components/CreatePost.jsx'
-import { PostFilter } from './components/PostFilter.jsx'
-import { PostSorting } from './components/PostSorting.jsx'
-
 import { useQuery } from '@tanstack/react-query'
-import { getPosts } from './api/posts.js'
+import { PostList } from '../components/PostList.jsx'
+import { CreatePost } from '../components/CreatePost.jsx'
+import { PostFilter } from '../components/PostFilter.jsx'
+import { PostSorting } from '../components/PostSorting.jsx'
+import { Header } from '../components/Header.jsx'
+import { getPosts } from '../api/posts.js'
 
 export function Blog() {
   const [author, setAuthor] = useState('')
@@ -16,12 +16,14 @@ export function Blog() {
     queryKey: ['posts', { author, sortBy, sortOrder }],
     queryFn: () => getPosts({ author, sortBy, sortOrder }),
   })
-
   const posts = postsQuery.data ?? []
 
   return (
     <div style={{ padding: 8 }}>
-      <h1>Welcome to my blog!</h1>
+      <Header />
+      <br />
+      <hr />
+      <br />
       <CreatePost />
       <br />
       <hr />
